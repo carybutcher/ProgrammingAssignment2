a712d4ce170da9b6c20cdcbacd8e8c75c379f719
Cary Butcher <caryrbutcher@gmail.com>
2015-05-24 18:55:58 -0400

    Programming Assignment #2
    Calculate and store inverse of square matrix to eliminate multiple calls
    to solve() function which takes cpu time.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these two functions work together to assign and store the inverse of a square
+## matrix and only call the solve function once if it is has already been solved
 
-## Write a short comment describing this function
+## returns a list of functions that allow the user to enter a square matrix through the set() function
+## with the inverse available through the get() function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmat <- function(solve) m <<- solve
+        getmat <- function() m
+        list(set = set, get = get,
+             setmat = setmat,
+             getmat = getmat)
 }
 
 
-## Write a short comment describing this function
+## returns the inverse of a square matrix, retrieving it from the getmat() function if it has already
+## been generated through the solve() function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmat()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmat(m)
+        m
 }
